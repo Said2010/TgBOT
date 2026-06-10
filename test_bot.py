a1cbb2bd2860ecd514bc40d9bea6dc285da6c38a
Said2010 <136082069+Said2010@users.noreply.github.com>
2026-06-10 08:58:16 +0300

    echo_bot

--- test_bot.py
+++ test_bot.py
@@ -1,7 +1,26 @@
 import os
 from dotenv import load_dotenv
-from aiogram import Bot
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message 
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message()
+async def echp(message):
+    await message.send_copy(chat_id=message.from_user.id)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        import asyncio
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
